f86ea2c6834484fd82dec7b397599c0e1466ade5
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-03-27 21:47:04 +0100

    Dodanie typu do modelu logu

--- MonitorLogow/Models/LogModel.cs
+++ MonitorLogow/Models/LogModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,19 @@ namespace MonitorLogow.Models
 {
     public class LogModel
     {
+        [Display(Name = "Log message")]
         public string Message { get; set; }
+        [Display(Name = "Log type")]
+        public LogTypes LogType { get; set; }
+
+        public enum LogTypes
+        {
+            Trace,
+            Debug,
+            Info,
+            Warn,
+            Error,
+            Fatal
+        }
     }
 }
